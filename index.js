a6b088fa525feef9ef6eb98d0fcdd82c4da192a8
Shefa <133137439+Shefaoudeen@users.noreply.github.com>
2023-08-16 16:48:22 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const pcBar = document.querySelector("#pc-bar");
 const mobBar = document.querySelector("#mobile-nav");
 const size = window.innerWidth;
-let sizeNum = document.querySelector("p");
-sizeNum.innerHTML = size;
+
 if (size < 800) {
   pcBar.classList.toggle("hidden");
   mobBar.classList.toggle("hidden");
