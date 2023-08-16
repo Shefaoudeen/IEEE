4c7bd887266927ff8cf3b107bc4caeb53e8c6192
Shefa <133137439+Shefaoudeen@users.noreply.github.com>
2023-08-16 16:55:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const mobBar = document.querySelector("#mobile-nav");
 const size = window.innerWidth;
 
 if (size < 800) {
-  pcBar.classList.toggle("hidden");
-  mobBar.classList.toggle("hidden");
+  pcBar.classList.add("hidden");
+  mobBar.classList.remove("hidden");
 }
